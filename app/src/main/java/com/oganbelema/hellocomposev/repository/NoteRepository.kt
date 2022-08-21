c14beb4d06dd52dc179082677e3da503a4c76dda
Oganbelema <oganbelema@gmail.com>
2022-08-21 17:09:49 +0100

    added repository for note data

---
+++ app/src/main/java/com/oganbelema/hellocomposev/repository/NoteRepository.kt
@@ -0,0 +1,28 @@
+package com.oganbelema.hellocomposev.repository
+
+import com.oganbelema.hellocomposev.dao.NoteDao
+import com.oganbelema.hellocomposev.model.Note
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class NoteRepository @Inject constructor(private val noteDao: NoteDao) {
+    suspend fun addNote(note: Note) = noteDao.insert(note)
+
+    suspend fun updateNote(note: Note) = noteDao.update(note)
+
+    suspend fun deleteNote(note: Note) = noteDao.delete(note)
+
+    suspend fun deleteAllNotes() = noteDao.deleteAllNotes()
+
+    suspend fun getNote(id: String) = noteDao.getNoteById(id)
+
+    fun getAllNotes(): Flow<List<Note>> {
+        return noteDao.getNotes()
+            .flowOn(Dispatchers.IO)
+            .conflate()
+    }
+
+}
