d4e96bcfa6270daee290b5f110de50ffb0b8ccb3
Oganbelema <oganbelema@gmail.com>
2022-08-21 16:14:15 +0100

    added coroutines suspend and flow to doa

--- app/src/main/java/com/oganbelema/hellocomposev/dao/NoteDao.kt
+++ app/src/main/java/com/oganbelema/hellocomposev/dao/NoteDao.kt
@@ -2,24 +2,25 @@ package com.oganbelema.hellocomposev.dao
 
 import androidx.room.*
 import com.oganbelema.hellocomposev.model.Note
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(note: Note)
+    suspend fun insert(note: Note)
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
-    fun update(note: Note)
+    suspend fun update(note: Note)
 
     @Query("SELECT * FROM note")
-    fun getNotes(): List<Note>
+    fun getNotes(): Flow<List<Note>>
 
     @Query("SELECT * FROM note WHERE id=:id")
-    fun getNoteById(id: String)
+    suspend fun getNoteById(id: String)
 
     @Delete
-    fun delete(note: Note)
+    suspend fun delete(note: Note)
 
     @Query("DELETE from note")
-    fun deleteAllNotes()
+    suspend fun deleteAllNotes()
 }
